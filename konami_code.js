f92b918e3b7da9059174b5dd703fcd6fe9ce5744
Keith Getty <keithgetty@yahoo.com>
2017-11-28 04:39:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init(){
   let index=0;
  	   window.addEventListener("keydown", function(event){
- } 		     const key=parseInt(event.which||event.detail);
+     } 		     const key=parseInt(event.which||event.detail);
      if(key===code[index]){
       index++;
       if(index===code.length){
